28ad1364b0b6cbf678bf84e0ff6a51b43f3e30a9
masikotimo <masikotimo@gmail.com>
2021-12-10 11:38:20 +0300

    adjust library action name

--- APIS/auth.py
+++ APIS/auth.py
@@ -1,12 +1,9 @@
-from wialon import Wialon, WialonError
+from wialon import WialonError, Wialon
 import json
 import requests
 
-wialon_api = Wialon()
-token = 'f0611136a2f64d810b638dd8ee6501ab1CAB947ADCE151C574FF1A5AC7DBF90BC06DCD79'
 
-
-def login():
+def login(wialon_api, token):
 
     try:
         result = wialon_api.token_login(
@@ -16,15 +13,15 @@ def login():
         # print(result['eid'])
 
         print('Login successfully')
+        return result['user']['crt']
 
-        wialon_api.core_logout()
     except WialonError as e:
         print(e)
 
 
-def logout():
+def logout(sid):
 
-    url = 'https://hst-api.wialon.com/wialon/ajax.html?svc=core/logout&params={}&sid={wialon_api.sid}'
+    url = 'https://hst-api.wialon.com/wialon/ajax.html?svc=core/logout&params={}&sid={sid}'
 
     try:
         request = requests.post(url)
@@ -35,5 +32,26 @@ def logout():
         print(e)
 
 
-login()
-logout()
+token = 'f0611136a2f64d810b638dd8ee6501ab1CAB947ADCE151C574FF1A5AC7DBF90BC06DCD79'
+
+
+wialon_api = Wialon()
+creator_id = login(wialon_api, token)
+params = {'creatorId': creator_id, 'name': 'ips_unitttss',
+          'hwTypeId': 96266, 'dataFlags': 1}
+action_name = 'core/create_unit'
+
+
+def create_unit():
+    try:
+        wialon_api.update_extra_params(params)
+        print('default params', wialon_api.__default_params)
+
+        wialon_api.call(action_name)
+        print("sucessfully")
+
+    except WialonError as e:
+        print(e)
+
+
+create_unit()
--- APIS/create_unit.py
+++ APIS/create_unit.py
@@ -1,34 +1,19 @@
+from .auth import login
+from . import token
 from wialon import Wialon, WialonError
-import json
 
-try:
-    wialon_api = Wialon()
-    # old username and password login is deprecated, use token login
-    token = 'f0611136a2f64d810b638dd8ee6501ab1CAB947ADCE151C574FF1A5AC7DBF90BC06DCD79'
-    result = wialon_api.token_login(
-        token=token)
+token = token.token
 
-    wialon_api.sid = result['eid']
-    # creator_id = result['user']['crt']
+wialon_api = Wialon()
+creator_id = login(wialon_api, token)
+params = {'creatorId': creator_id, 'name': 'ips_unitttss',
+          'hwTypeId': 96266, 'dataFlags': 1}
+action_name = 'core/create_unit'
 
-    # params = {'creatorId': creator_id, 'name': 'ips_unitttss',
-    #           'hwTypeId': 96266, 'dataFlags': 1}
 
-    # action_name = 'svc=core/create_unit'
-    # url = 'https://hst-api.wialon.com/wialon/ajax.html'
-    # # params = {
-    # #         'svc': action_name.replace('_', '/', 1),
-    # #         'params': params.encode('utf-8'),
-    # #         'sid': sid
-    # #     }
+def create_unit():
+    wialon_api.__default_params = params
+    wialon_api.call(action_name)
 
-    # res = wialon_api.request(action_name, url, params)
 
-    # resultx = wialon_api.avl_evts()
-
-    # print(json.dumps(result))
-    print(result)
-
-    wialon_api.core_logout()
-except WialonError as e:
-    print(e)
+create_unit()
